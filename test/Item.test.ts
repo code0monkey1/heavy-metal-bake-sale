5ed78c762872048e91a12460bca3e7fc8becc866
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-19 11:08:00 +0000

    19 Jun 2023, 16:38

--- test/Item.test.ts
+++ test/Item.test.ts
@@ -28,5 +28,33 @@ describe('Item', () => {
          })
 
       })
+
+         describe(' two items', () => {
+           
+         it.each([
+          {items:'B,C',price:2.1},
+          {items:'M,B',price:1.75},
+          {items:'B,W',price:2.25},
+          {items:'W,C',price:2.85},
+        ])('item $item bought for $price',({items,price})=>{
+             
+            //Arrange
+            const record :Record<string,Item> ={
+               'B': new Item("Brownie","B",48,0.75),
+               'M': new Item("Muffin","M",36,1),
+               'C': new Item("Cake Pops","C",24,1.35),
+               'W' : new Item('Water','W',30,1.50)
+            }
+
+            //Act 
+            const amount1 = record[items[0]].getPrice()
+            const amount2 = record[items[1]].getPrice()
+
+            //Assert
+            expect(amount1+amount2).toBe(price)
+
+         })
+
+      })
       
 })
