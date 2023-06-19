c5f61be07413028c48a6b1a5fdb6b5f392e0de42
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-19 10:52:04 +0000

    19 Jun 2023, 16:22

---
+++ test/Item.test.ts
@@ -0,0 +1,18 @@
+describe('Item', () => {
+       
+      describe('single item', () => {
+           
+         it.each([
+          {item:'B',price:0.75},
+          {item:'M',price:1},
+          {item:'C',price:1.35},
+          {item:'W',price:1.50},
+        ])('item $item bought for $price',()=>{
+             
+             
+
+         })
+
+      })
+      
+})
