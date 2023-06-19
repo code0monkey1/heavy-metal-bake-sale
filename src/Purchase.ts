bd9ae17ba09106577370a795cede94ec2b50b42a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-19 11:40:14 +0000

    19 Jun 2023, 17:10

--- src/Purchase.ts
+++ src/Purchase.ts
@@ -18,14 +18,38 @@ export default class Purchase{
          let total = 0
           
          const itemList = items.split(',')
-          
-          itemList.forEach(item =>{
+
+         itemList.forEach(item =>{
 
               total+= this.store[item].getPrice()
           })
      
           return total
-    }
+     }
+     
+      hasItem(){
+
+      }
+
+}
+
+class Store{
+    
+      private store :Record<string,Item>={
+               'B': new Item("Brownie","B",48,0.75),
+               'M': new Item("Muffin","M",36,1),
+               'C': new Item("Cake Pops","C",24,1.35),
+               'W' : new Item('Water','W',30,1.50)
+            };
+      
+      initialize(items:Record<string,Item> ){
+         this.store=items
+      }
 
 
+      hasItem(item:string){
+            
+           this.store[item].getQuantity()>0
+
+      }
 }
