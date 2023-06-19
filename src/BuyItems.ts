f8dc53de261a9e2bdbb304e26ec4b1614e5480d0
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-19 10:34:14 +0000

    19 Jun 2023, 16:04

--- src/BuyItems.ts
+++ src/BuyItems.ts
@@ -27,7 +27,7 @@ export default class BuyItems implements ISale {
       if(!item)
         throw new Error(`Item ${items[0]} not found`)
       
-      return -1
+      return item.getQuantity()
   }
   balance(): void {
     throw new Error("Method not implemented.");
