4a4924acebe644a7aa2a5b0f133d7b8046ecb6ff
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-19 11:28:13 +0000

    19 Jun 2023, 16:58

--- test/Item.test.ts
+++ test/Item.test.ts
@@ -1,51 +1,56 @@
 import Item from "../src/Item"
 import Purchase from "../src/Purchase"
 
-describe('Item', () => {
-         
-      describe('single item', () => {
-           
-         it.each([
-          {item:'B',price:0.75},
-          {item:'M',price:1},
-          {item:'C',price:1.35},
-          {item:'W',price:1.50},
-        ])('item $item bought for $price',({item,price})=>{
+describe('Purchase', () => {
+        
+     describe('when all items are in stock', () => {
+       
+        describe('single item', () => {
              
-            //Arrange
-             const purchaseItems = new Purchase()
-
-            //Act 
-            const amount = purchaseItems.getTotal(item)
-
-            //Assert
-            expect(amount).toBe(price)
-
-         })
-
-      })
-
-         describe('two items', () => {
-           
-         it.each([
-          {items:'B,C',price:2.1},
-          {items:'M,B',price:1.75},
-          {items:'B,W',price:2.25},
-          {items:'W,C',price:2.85},
-        ])('item $items bought for $price',({items,price})=>{
-         //Arrange
-             const purchaseItems = new Purchase()
-
-            //Act 
-            const amount = purchaseItems.getTotal(items)
-
-            //Assert
-            expect(amount).toBe(price)
-
-      })
-
-
+           it.each([
+            {item:'B',price:0.75},
+            {item:'M',price:1},
+            {item:'C',price:1.35},
+            {item:'W',price:1.50},
+          ])('item $item bought for $price',({item,price})=>{
+               
+              //Arrange
+               const purchaseItems = new Purchase()
+  
+              //Act 
+              const amount = purchaseItems.getTotal(item)
+  
+              //Assert
+              expect(amount).toBe(price)
+  
+           })
+  
+        })
+  
+           describe('two items', () => {
+             
+           it.each([
+            {items:'B,C',price:2.1},
+            {items:'M,B',price:1.75},
+            {items:'B,W',price:2.25},
+            {items:'W,C',price:2.85},
+          ])('item $items bought for $price',({items,price})=>{
+           //Arrange
+               const purchaseItems = new Purchase()
+  
+              //Act 
+              const amount = purchaseItems.getTotal(items)
+  
+              //Assert
+              expect(amount).toBe(price)
+  
+        })
+  
+  
+        
       
-})
+       })
+       
+     })
 
 })
