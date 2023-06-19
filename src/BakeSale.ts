279297cd402973bd79b242af45c407632c6f8145
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-19 13:35:23 +0000

    19 Jun 2023, 19:05

--- src/BakeSale.ts
+++ src/BakeSale.ts
@@ -55,7 +55,7 @@ class Item {
   constructor(
     private readonly name: string,
     private readonly purchaseCode: string,
-    private quantity: number,
+    private  quantity: number,
     private readonly price: number
   ) {}
 
