9bf0e887ab7fbc4eaa21dae774f78ae3fcadfd6c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-19 11:35:19 +0000

    19 Jun 2023, 17:05

--- src/Purchase.ts
+++ src/Purchase.ts
@@ -1,21 +1,24 @@
-import Item from "./Item"
+import Item from "./Item";
 
 export default class Purchase{
      
-      private store :Record<string,Item> ={
+      private store :Record<string,Item>={
                'B': new Item("Brownie","B",48,0.75),
                'M': new Item("Muffin","M",36,1),
                'C': new Item("Cake Pops","C",24,1.35),
                'W' : new Item('Water','W',30,1.50)
-            }
- 
+            };
+      
+      initialize(items:Record<string,Item> ){
+         this.store=items
+      }
      
       getTotal(items:string) {
     
          let total = 0
           
          const itemList = items.split(',')
-
+          
           itemList.forEach(item =>{
 
               total+= this.store[item].getPrice()
--- test/Item.test.ts
+++ test/Item.test.ts
@@ -57,14 +57,21 @@ describe('Purchase', () => {
              
            it.each([
             {item:'B',price:0.75},
-            {item:'M',price:1},
-            {item:'C',price:1.35},
-            {item:'W',price:1.50},
+            // {item:'M',price:1},
+            // {item:'C',price:1.35},
+            // {item:'W',price:1.50},
           ])('item $item should be out of stock',({item,price})=>{
                
               //Arrange
                const purchaseItems = new Purchase()
-  
+
+               purchaseItems.initialize({
+               'B': new Item("Brownie","B",0,0.75),
+               'M': new Item("Muffin","M",0,1),
+               'C': new Item("Cake Pops","C",0,1.35),
+               'W' : new Item('Water','W',0,1.50)
+              })
+              
               //Act 
               const amount = purchaseItems.getTotal(item)
   
