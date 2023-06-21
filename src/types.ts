2034ca0893be0dd220b716d5596b341aef2e8416
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-21 06:48:42 +0000

    21 Jun 2023, 12:18

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Item={
+     name:string,
+     quantity:number,
+     price :number,
+     code:string  
+}
+
+export type Sale={
+
+  makeSale:(items:Item[],amount:number)=>string
+  
+}
