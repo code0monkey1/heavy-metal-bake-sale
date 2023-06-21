30a6bd83649ae27bb11ba5b070a2f357cb1bae7f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-21 06:01:22 +0000

    21 Jun 2023, 11:31

--- src/Item.ts
+++ src/Item.ts
@@ -1,16 +1,19 @@
-export default class Item{
+import { TItem } from "./types";
+
+class Item implements TItem{
 
   constructor(
-      private readonly name:string,
-      private readonly purchaseCode:string,
-      private quantity:number,
-      private readonly price:number
-    ){}
+    private readonly name: string,
+    private readonly purchaseCode: string,
+    private  quantity: number,
+    private readonly price: number
+  ) {}
 
   getName(): string {
     return this.name;
   }
 
+ 
   getPurchaseCode(): string {
     return this.purchaseCode;
   }
@@ -21,13 +24,5 @@ export default class Item{
 
   getPrice(): number {
     return this.price;
-  }
-
-  decrementQuantity():void{
-    this.quantity-= 1
-  }
-  
-  incrementQuantity():void{
-    this.quantity+=1
   }
 }
