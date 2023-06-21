0df289b0dbccd95872eb527f65fd137513813aec
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-21 06:16:00 +0000

    21 Jun 2023, 11:46

--- test/Item.test.ts
+++ test/Item.test.ts
@@ -1,11 +1,16 @@
+import { BakeSale } from "../src/BakeSale"
+
 describe('BakeSale', () => {
 
      describe('when all items are in stock',()=>{
 
         test('only 1 item',()=>{
+           
+           const sale = new BakeSale()
+
+           sale.runSale(['B'],29)
 
 
-         
         })
 
      })
