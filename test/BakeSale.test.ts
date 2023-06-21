aaf2158088e06a8802112a491d2b77e489f3141a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-21 07:09:02 +0000

    21 Jun 2023, 12:39

--- test/BakeSale.test.ts
+++ test/BakeSale.test.ts
@@ -1,3 +1,5 @@
+import { BakeSale } from "../src/BakeSale"
+
 describe('BakeSale', () => {
      
     describe('When all items are in stock',()=>{
@@ -5,7 +7,18 @@ describe('BakeSale', () => {
      
         it('single item',()=>{
 
-             
+               //Arrange
+               const bakeSale = new BakeSale()
+               const items=['B']
+               const amount=10
+               const balance=9.25
+
+               //Act 
+               const result =bakeSale.makeSale(items,amount)
+              
+
+               //Assert
+               expect(result).toBe(`The balance is $${balance.toFixed(2)}`)
 
         })
 
