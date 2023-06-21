f30c228ebd30746562a36008df2d0e1ba7a88c24
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-21 06:31:39 +0000

    21 Jun 2023, 12:01

--- test/Item.test.ts
+++ test/Item.test.ts
@@ -1,9 +1,11 @@
 import { BakeSale } from "../src/BakeSale"
+import { IStore, TItem } from "../src/types"
 
 describe('BakeSale', () => {
 
      describe('when all items are in stock',()=>{
-
+         
+      
         test
         .each([
          {value:['B'],amount:10,result:9.25},
@@ -12,8 +14,17 @@ describe('BakeSale', () => {
          {value:['W'],amount:10,result:8.50},
       ])
         ('only 1 item',({value,amount,result})=>{
+
+          const mockStore :IStore = {
+
+             items: [],
+             initialize: function (items: TItem[]): void {
+                throw new Error("Function not implemented.")
+             }
+          }
+
            
-           const sale = new BakeSale()
+           const sale = new BakeSale(mockStore)
 
            const response= sale.runSale(value,amount)
           
