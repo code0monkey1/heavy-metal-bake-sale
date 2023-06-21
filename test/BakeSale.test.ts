f1b43dd160fdbdf22e4441ef391293b11df36284
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-21 07:16:51 +0000

    21 Jun 2023, 12:46

--- test/BakeSale.test.ts
+++ test/BakeSale.test.ts
@@ -5,7 +5,10 @@ describe('BakeSale', () => {
     describe('When all items are in stock',()=>{
 
         it.each([
-          {items:['B'],amount:10,balance:9.25}
+          {items:['B'],amount:10,balance:9.25},
+          {items:['M'],amount:10,balance:9.00},
+          {items:['C'],amount:10,balance:8.65},
+          {items:['W'],amount:10,balance:8.50},
         ])('for single item $items ,when given amount $amount ,should return a balance of $$balance',({items,amount,balance})=>{
 
                //Arrange
